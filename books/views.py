42e72dff60b2f0f1dc64819825a0e08fe45b835e
Nidhi-Sharma-csit <nidhi1912002@akgec.ac.in>
2020-09-13 15:25:04 +0530

    added all functionalities in views

--- books/views.py
+++ books/views.py
@@ -18,3 +18,26 @@ def upload(request):
             return HttpResponse('<h1>your form is wrong,reload on <a href = {% url "index" %}> reload</a> </h1>')
     else:
         return render(request, 'books/upload_form.html', {'upload_form':upload })
+
+def update_book(request, book_id):
+    book_id = int(book_id)
+    try:
+        Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        return redirect('index')
+    book_form = BookCreate(request.POST or none, instance=book_sel)
+    if book_form.is_valid:
+        book_form.save()
+        return redirect('index')
+    return render(request, 'book/upload_form.html', { 'upload_form':book_form })
+
+def delete_book(reqeust, book_id):
+    book_id = int(book_id)
+    try: 
+        Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        return redirect('index')
+    book_sel.delete()
+    return redirect('index')
+    
+
--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-
+from books import views
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload/', views.upload, name='upload-book'),
+    path('<int:book_id>/edit/', views.update_book, name='update'),
+    path('<int:book_id>/delete/', views.delete_book, name="delete"),
     path('admin/', admin.site.urls),
 ]
